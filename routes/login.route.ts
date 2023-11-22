47d12412dcaee5682d2d1fe5003505f77870a025
p1err00 <pierro.adrien.simon@gmail.com>
2023-11-22 11:49:15 +0100

    ADD: Add auth routes

--- routes/login.route.ts
+++ routes/login.route.ts
@@ -1,23 +1,30 @@
 import express from "express";
 import usersSchema from '../models/users.model'
+import { secret } from '../config/secret.js'
+import jsonwebtoken from 'jsonwebtoken'
+// import bcrypt from 'bcrypt'
 
-export default function(app: any) {
-  const jsonMiddleware = express.json();
-  
-  app.post('/login', jsonMiddleware, (req, res) => {
 
-    const newUser = new usersSchema({
-      username: 'michel',
-      firstname: 'Mich',
-      lastname: 'Hell',
-      password: 'qweasd',
-      email: 'qweqwe@qwe.fr',
-      is_artisant: false,
-      avatar: 'qweqewqweqweqwe',
-      id_prospect: 'qwe1as4sadasdad556a8sd8d'
-    });
+export default function (app: any) {
 
-    newUser.save();
-    res.json("message")
-  });
+	const jsonMiddleware = express.json();
+
+	app.post('/login', jsonMiddleware, (req, res) => {
+
+		if (!req.body.username || !req.body.password) return res.status(400).json({ message: 'Il manque un truc' });
+
+		try {
+			const user = usersSchema.findOne(u => u.username === req.body.username && u.password === req.body.password);
+			if (!user) return res.status(400).json({ message: 'User pas trouve' });
+
+			const token = jsonwebtoken.sign({
+				id: user['id'],
+				username: user['username']
+			}, secret, { expiresIn: '3 hours' });
+
+			res.json({ access_token: token });
+		} catch (error) {
+			res.json({ access_token: error });
+		}
+	});
 }
