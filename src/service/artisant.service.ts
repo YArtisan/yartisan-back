e7a6ea637693099665c7a67c98885a0df579fd58
delhadjm <cirelaria@gmail.com>
2023-12-13 09:08:23 +0100

    creation des routes controller et verification pour l'artisant

--- src/service/artisant.service.ts
+++ src/service/artisant.service.ts
@@ -1,7 +1,7 @@
 import artisanSchema from "./../../models/artisant.model";
 import { createArtisantdto } from "../dto/artisant.dto";
 
-async function createUserService(
+async function createArtisantService(
   request: createArtisantdto,
   res: any
 ): Promise<void> {
@@ -34,3 +34,60 @@ async function createUserService(
     res.status(200).json("Save into Db !");
   }
 }
+
+async function updateArtisantService(request: any, res: any): Promise<void> {
+  try {
+    const userId = request.userId;
+
+    const updatedData = {
+      userid: Number,
+      compagny_name: request.compagny_name,
+      phone_number: request.phone_number,
+      profile_picture: request.profile_picture,
+      job_description: request.job_description,
+      average_price: request.average_price,
+      number_of_employees: request.number_of_employees,
+      isVisible: request.isVisible,
+      created_at: request.created_at,
+      last_update: request.last_update,
+    };
+
+    await artisanSchema.findOneAndUpdate({ id: userId }, updatedData);
+
+    res.status(200).json("Artisant updated successfully");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function deleteArtisantService(request: any, res: any): Promise<void> {
+  try {
+    const userId = request.userId;
+
+    // Supprimer l'artisan
+    await artisanSchema.deleteOne({ _id: userId });
+
+    res.status(200).json("Artisan supprimé avec succès");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function getArtisantDataService(request: any, res: any): Promise<void> {
+  try {
+    const userId = request.userId;
+
+    const artisantData = await artisanSchema.findOne({ _id: userId });
+
+    if (!artisantData) {
+      res.status(404).json("Artisan non trouvé");
+      return;
+    }
+
+    res.status(200).json(artisantData);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export default { createArtisantService, updateArtisantService, deleteArtisantService, getArtisantDataService };
