88108fa20989b24a45e0b8cf9c3b24342255233b
MatthiasChometon <matthiaschometon787@gmail.com>
2024-01-12 11:18:39 +0100

    fix: add return

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -6,7 +6,10 @@ import { auth } from '../../main.js';
 export const authMiddleware = async (req: any, res: Response, next: NextFunction): Promise<void> => {
     const authorization = req.headers.authorization ?? '';
     const idToken = authorization.split(' ')[1] ?? '';
-    if (idToken == null || idToken === '') next();
+    if (idToken == null || idToken === '') {
+        next();
+        return
+    }
     const decodedToken = await auth.verifyIdToken(idToken);
     const user = await usersSchema.findOne({ email: decodedToken.email });
     req.user = user;
