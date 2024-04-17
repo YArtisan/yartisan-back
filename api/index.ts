042436ab4b35217b45f0e103f400c204458228e0
Thomasperge <thomaskauffmant74@gmail.com>
2024-04-17 11:44:41 +0200

    Firebase .env

--- api/index.ts
+++ api/index.ts
@@ -18,14 +18,24 @@ import conversationRoute from "../src/routes/conversation.route.js";
 
 const app = express();
 
-const adminCred: admin.ServiceAccount = {
-  projectId: process.env.project_id,
-  privateKey: process.env.private_key,
-  clientEmail: process.env.client_email,
+const adminCred =
+{
+  "type": "service_account",
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": process.env.FIREBASE_CERT_URL,
+  "universe_domain": "googleapis.com"
 }
 
+
 admin.initializeApp({
-  credential: admin.credential.cert(adminCred),
+  credential: admin.credential.cert(adminCred as admin.ServiceAccount),
 });
 
 export const auth = admin.auth();
